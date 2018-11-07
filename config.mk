8e67f33798db32f4996a643fe1978d124cfd4412
Ticean Bennett <ticean@reactioncommerce.com>
2018-11-06 16:11:56 -0800

    Extract config from Makefile and allow override
    Renames configuration variable names to be more generic. Subprojects may
    be official Reaction projects but you are free to add your own.
    
    Moves configuration to the `config.mk` file which is included in the
    Makefile. This provides an opportunity to include multiple files in
    which configuration variables can be redefined.
    
    As an alternative implementation, it would be possible to keep the
    configuration in the main Makefile and include the secondary config.
    This would be functionally equivalent. I think extracting both is better
    as it provides a documented example for the configuration file.
    
    The project's `config.mk` file can be copied to `config.local.mk` and
    modified.

---
+++ config.mk
@@ -0,0 +1,41 @@
+###############################################################################
+### Reaction Platform Configuration
+###
+### This file defines configuration used in the Makefile.
+### You may add and/or override these values with your own custom configuration
+### in `config.local.mk`.
+###
+### Please see GNU Makes multi-line variable documentation for more info.
+### https://www.gnu.org/software/make/manual/html_node/Multi_002dLine.html
+###############################################################################
+
+
+# List of tools that must be installed.
+# A simple check to determine the tool is available. No version check, etc.
+define REQUIRED_SOFTWARE =
+docker
+docker-compose
+git
+node
+yarn
+endef
+
+# Defined here are the subprojects in a comma-separated format
+# GIT_REPO_URL,SUBDIR_NAME,TAG
+# GIT_REPO_URL is the URL of the git repository
+# SUBDIR_NAME is just the directory name itself
+# TAG is the git tag or branch to checkout
+# Projects will be started in this order
+define SUBPROJECT_REPOS
+git@github.com:/reactioncommerce/reaction-hydra.git,reaction-hydra,master \
+git@github.com:/reactioncommerce/reaction.git,reaction,master \
+git@github.com:/reactioncommerce/reaction-next-starterkit.git,reaction-next-starterkit,master
+endef
+
+# List of user defined networks that should be created.
+define DOCKER_NETWORKS =
+auth.reaction.localhost \
+api.reaction.localhost
+endef
+
+HOOK_DIR=.reaction/project-hooks
