adfe7038fd5c9f417d711a6877afdf3ecd9948f0
Ross Hadden <rosshadden@gmail.com>
2019-12-10 15:41:49 -0500

    feat: add reaction-admin and reaction-identity
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- config.mk
+++ config.mk
@@ -27,9 +27,11 @@ endef
 # TAG is the git tag or branch to checkout
 # Projects will be started in this order
 define SUBPROJECT_REPOS
+git@github.com:/reactioncommerce/example-storefront.git,example-storefront,v3.0.0-alpha \
+git@github.com:/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-alpha \
 git@github.com:/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0-alpha \
-git@github.com:/reactioncommerce/reaction.git,reaction,v3.0.0-alpha \
-git@github.com:/reactioncommerce/example-storefront.git,example-storefront,v3.0.0-alpha
+git@github.com:/reactioncommerce/reaction-indentity.git,reaction-indentity,v3.0.0-alpha \
+git@github.com:/reactioncommerce/reaction.git,reaction,v3.0.0-alpha
 endef
 
 # List of user defined networks that should be created.
