571acb985168c38ffb57b617604de5af3e4101fd
Erik Kieckhafer <ek@ato.la>
2020-04-08 11:36:07 -0700

    feat: update development platform to sync with other releases
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- config/reaction-oss/reaction-v3.1.0.mk
+++ config/reaction-oss/reaction-v3.1.0.mk
@@ -1,14 +1,8 @@
 ###############################################################################
-### Reaction Platform Configuration
+### Reaction OSS v3.0.4
 ###
-### This file defines configuration used in the Makefile.
-### You may add and/or override these values with your own custom configuration
-### in `config.local.mk`.
-###
-### Please see GNU Makes multi-line variable documentation for more info.
-### https://www.gnu.org/software/make/manual/html_node/Multi_002dLine.html
+### See: `/config.mk` for documentation.
 ###############################################################################
-
 
 # List of tools that must be installed.
 # A simple check to determine the tool is available. No version check, etc.
@@ -28,9 +22,9 @@ endef
 # Projects will be started in this order
 define SUBPROJECT_REPOS
 https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
-https://github.com/reactioncommerce/reaction.git,reaction,v3.5.0 \
+https://github.com/reactioncommerce/reaction.git,reaction,v3.4.0 \
 https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.0.0 \
-https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.6 \
+https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.5 \
 https://github.com/reactioncommerce/example-storefront.git,example-storefront,v3.0.0
 endef
 
