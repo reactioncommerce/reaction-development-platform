d66e2e70dbf9ffe3f0ff7624f65a57813bb646be
Eric Dobbertin <eric@dairystatedesigns.com>
2020-04-29 15:23:53 -0500

    feat: update plugin repo names
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- config.mk
+++ config.mk
@@ -42,8 +42,11 @@ https://github.com/reactioncommerce/api-core.git \
 https://github.com/reactioncommerce/api-plugin-accounts.git \
 https://github.com/reactioncommerce/api-plugin-address-validation-test.git \
 https://github.com/reactioncommerce/api-plugin-address-validation.git \
+https://github.com/reactioncommerce/api-plugin-authentication.git \
+https://github.com/reactioncommerce/api-plugin-authorization-simple.git \
 https://github.com/reactioncommerce/api-plugin-carts.git \
 https://github.com/reactioncommerce/api-plugin-catalogs.git \
+https://github.com/reactioncommerce/api-plugin-discounts-codes.git \
 https://github.com/reactioncommerce/api-plugin-discounts.git \
 https://github.com/reactioncommerce/api-plugin-email-smtp.git \
 https://github.com/reactioncommerce/api-plugin-email-templates.git \
@@ -53,8 +56,10 @@ https://github.com/reactioncommerce/api-plugin-i18n.git \
 https://github.com/reactioncommerce/api-plugin-inventory-simple.git \
 https://github.com/reactioncommerce/api-plugin-inventory.git \
 https://github.com/reactioncommerce/api-plugin-job-queue.git \
+https://github.com/reactioncommerce/api-plugin-navigation.git \
 https://github.com/reactioncommerce/api-plugin-notifications.git \
 https://github.com/reactioncommerce/api-plugin-orders.git \
+https://github.com/reactioncommerce/api-plugin-payments-example.git \
 https://github.com/reactioncommerce/api-plugin-payments-stripe.git \
 https://github.com/reactioncommerce/api-plugin-payments.git \
 https://github.com/reactioncommerce/api-plugin-pricing-simple.git \
@@ -64,18 +69,13 @@ https://github.com/reactioncommerce/api-plugin-shipments-flat-rate.git \
 https://github.com/reactioncommerce/api-plugin-shipments.git \
 https://github.com/reactioncommerce/api-plugin-shops.git \
 https://github.com/reactioncommerce/api-plugin-simple-schema.git \
+https://github.com/reactioncommerce/api-plugin-sitemap-generator.git \
 https://github.com/reactioncommerce/api-plugin-surcharges.git \
+https://github.com/reactioncommerce/api-plugin-system-information.git \
 https://github.com/reactioncommerce/api-plugin-tags.git \
 https://github.com/reactioncommerce/api-plugin-taxes-flat-rate.git \
 https://github.com/reactioncommerce/api-plugin-taxes.git \
-https://github.com/reactioncommerce/plugin-authentication.git \
-https://github.com/reactioncommerce/plugin-discount-codes.git \
-https://github.com/reactioncommerce/plugin-navigation.git \
-https://github.com/reactioncommerce/plugin-payments-example.git \
-https://github.com/reactioncommerce/plugin-simple-authorization.git \
-https://github.com/reactioncommerce/plugin-sitemap-generator.git \
-https://github.com/reactioncommerce/plugin-system-information.git \
-https://github.com/reactioncommerce/plugin-translations.git
+https://github.com/reactioncommerce/api-plugin-translations.git
 endef
 
 # List of user defined networks that should be created.
