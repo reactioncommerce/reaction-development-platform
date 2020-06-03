15580f20871a2d6f1fdf9a6b3739c5376521497a
Peter Lyons <pete.lyons@mailchimp.com>
2020-06-03 16:01:30 -0600

    feat: Add example-storefront v3.1.0 to this release

--- config/reaction-oss/reaction-v3.8.0.mk
+++ config/reaction-oss/reaction-v3.8.0.mk
@@ -31,7 +31,7 @@ https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
 https://github.com/reactioncommerce/reaction.git,reaction,v3.8.0 \
 https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.0.0 \
 https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.7 \
-https://github.com/reactioncommerce/example-storefront.git,example-storefront,v3.0.0
+https://github.com/reactioncommerce/example-storefront.git,example-storefront,v3.1.0
 endef
 
 # These are all the plugins that `make clone-api-plugins` will clone.
