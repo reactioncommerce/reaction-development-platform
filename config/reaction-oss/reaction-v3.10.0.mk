e89243a67c466aa7b11046ca12bb08d5e10d8a48
Kris <KristoferSoto@outlook.com>
2020-10-29 11:15:02 -0400

    Changed config to pull our forked repo's, created Noa master branch

--- config/reaction-oss/reaction-v3.10.0.mk
+++ config/reaction-oss/reaction-v3.10.0.mk
@@ -22,9 +22,9 @@ endef
 # Projects will be started in this order
 define SUBPROJECT_REPOS
 https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
-https://github.com/reactioncommerce/reaction.git,reaction,v3.10.0 \
-https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.3.0 \
-https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.10 \
+https://github.com/Project-Noa-Inc/reaction.git,v3.10.0 \
+https://github.com/Project-Noa-Inc/reaction-identity.git,reaction-identity,v3.3.0 \
+https://github.com/Project-Noa-Inc/reaction-admin.git,v3.0.0-beta.10 \
 https://github.com/reactioncommerce/example-storefront.git,example-storefront,v4.0.0
 endef
 
