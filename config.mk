52280a3b8b69e69f1fdbc6b8ff09a4959509ff3e
Akarshit Wal <akarshitwal@gmail.com>
2021-08-03 10:53:00 -0600

    ci: v3.14.0
    Signed-off-by: Akarshit Wal <akarshitwal@gmail.com>

--- config.mk
+++ config.mk
@@ -28,10 +28,10 @@ endef
 # Projects will be started in this order
 define SUBPROJECT_REPOS
 https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
-https://github.com/reactioncommerce/reaction.git,reaction,v3.13.0 \
-https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.15 \
+https://github.com/reactioncommerce/reaction.git,reaction,v3.13.1 \
+https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.18 \
 https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.3.0 \
-https://github.com/reactioncommerce/example-storefront.git,example-storefront,v4.0.2
+https://github.com/reactioncommerce/example-storefront.git,example-storefront,v4.1.0
 endef
 
 # These are all the plugins that `make clone-api-plugins` will clone.
