d6d3b0ca6b3da74f4e613e987fd505b727909625
Akarshit Wal <akarshitwal@gmail.com>
2021-08-11 13:16:17 -0600

    feat: breaking use account-js for authentication
    BREAKING CHANGE: the authorization methods have been switched to account-js
    
    Signed-off-by: Akarshit Wal <akarshitwal@gmail.com>

--- config.mk
+++ config.mk
@@ -27,11 +27,11 @@ endef
 # TAG is the git tag or branch to checkout
 # Projects will be started in this order
 define SUBPROJECT_REPOS
-https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
+
 https://github.com/reactioncommerce/reaction.git,reaction,v3.13.2 \
 https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.19 \
-https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.3.1 \
 https://github.com/reactioncommerce/example-storefront.git,example-storefront,v4.1.0
+
 endef
 
 # These are all the plugins that `make clone-api-plugins` will clone.
